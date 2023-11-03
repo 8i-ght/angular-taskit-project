eb6e42a4b996ae717e658b44a38d39529be35c08
CTC CCP 6 <113914772+8i-ght@users.noreply.github.com>
2023-11-03 14:37:32 -0500

    need to fix adding task

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
-
+export class TodoComponent{
+  taskList: any[] = []
+  newTodoForm = this.formBuilder.group({
+    todoItem: ''
+  })
+    
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+  
+  addTask() {
+    const value = this.newTodoForm.value.todoItem
+    this.taskList.push({ id: this.taskList.length, name: value })
+    this.newTodoForm.reset(); 
+  }
 }
