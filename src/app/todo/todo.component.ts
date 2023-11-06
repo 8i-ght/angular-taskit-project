ed7b893adfe71d8a95f4405aeeb0ba11568c0c24
CTC CCP 6 <113914772+8i-ght@users.noreply.github.com>
2023-11-06 12:43:17 -0600

    finish project minus local storage

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -18,6 +19,21 @@ export class TodoComponent{
   addTask() {
     const value = this.newTodoForm.value.todoItem
     this.taskList.push({ id: this.taskList.length, name: value })
+    window.localStorage.setItem('task', JSON.stringify(this.taskList))
     this.newTodoForm.reset(); 
+  }
+
+  completed: boolean = false;
+
+  markDone(value: any) {
+    value.completed = !value.completed
+    value.completed === true ?
+      this.taskList.push(this.taskList.splice(this.taskList.indexOf(value), 1) [0]) :
+      this.taskList.unshift(this.taskList.splice(this.taskList.indexOf(value), 1) [0])
+  }
+
+  removeTask(i: any) {
+    this.taskList.splice(i, 1)
+    window.localStorage.setItem('task', JSON.stringify(this.taskList))
   }
 }
